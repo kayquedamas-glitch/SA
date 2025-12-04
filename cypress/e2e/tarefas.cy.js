3119f3710a18078ff85992acc09ec83a7bb2aacc
kayque <kayquedamas@gmail.com>
2025-12-03 21:19:28 -0300

    ok

--- cypress/e2e/tarefas.cy.js
+++ cypress/e2e/tarefas.cy.js
@@ -9,16 +9,26 @@ describe('Funcionalidade do Protocolo Diário', () => {
     cy.get('.task-item').should('have.length', 3)
   })
 
-  it('Deve marcar e desmarcar uma tarefa visualmente', () => {
-    cy.get('.task-item').first().as('primeiraTarefa')
+  it('Deve marcar e desmarcar TODAS as tarefas da lista', () => {
+    // O comando .each() pega todos os elementos encontrados (3 tarefas)
+    // e executa os passos abaixo para cada um deles, um de cada vez.
+    cy.get('.task-item').each(($tarefa) => {
+      
+      // 1. Clica na tarefa atual
+      // Usamos cy.wrap($tarefa) para trazer o elemento HTML para dentro dos comandos do Cypress
+      cy.wrap($tarefa).click()
+      
+      // 2. Verifica se marcou (se ganhou a classe 'concluida' e o ícone ficou vermelho)
+      cy.wrap($tarefa).should('have.class', 'concluida')
+      cy.wrap($tarefa).find('i').should('have.class', 'text-red-500')
 
-    cy.get('@primeiraTarefa').click()
-    cy.get('@primeiraTarefa').should('have.class', 'concluida')
-    cy.get('@primeiraTarefa').find('i').should('have.class', 'text-red-500')
+      // 3. Clica de novo na mesma tarefa para desmarcar
+      cy.wrap($tarefa).click()
 
-    cy.get('@primeiraTarefa').click()
-    cy.get('@primeiraTarefa').should('not.have.class', 'concluida')
-    cy.get('@primeiraTarefa').find('i').should('not.have.class', 'text-red-500')
+      // 4. Verifica se desmarcou (se perdeu a classe e o ícone voltou ao normal)
+      cy.wrap($tarefa).should('not.have.class', 'concluida')
+      cy.wrap($tarefa).find('i').should('not.have.class', 'text-red-500')
+    })
   })
 
 })
