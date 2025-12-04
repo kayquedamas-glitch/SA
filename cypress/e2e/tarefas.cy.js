bb804822be07343aa3fd2133f32cea167251ae5a
kayque <kayquedamas@gmail.com>
2025-12-03 21:22:04 -0300

    pronto

--- cypress/e2e/tarefas.cy.js
+++ cypress/e2e/tarefas.cy.js
@@ -10,22 +10,23 @@ describe('Funcionalidade do Protocolo Diário', () => {
   })
 
   it('Deve marcar e desmarcar TODAS as tarefas da lista', () => {
-    // O comando .each() pega todos os elementos encontrados (3 tarefas)
-    // e executa os passos abaixo para cada um deles, um de cada vez.
+
+    
     cy.get('.task-item').each(($tarefa) => {
       
-      // 1. Clica na tarefa atual
-      // Usamos cy.wrap($tarefa) para trazer o elemento HTML para dentro dos comandos do Cypress
+
       cy.wrap($tarefa).click()
+
       
-      // 2. Verifica se marcou (se ganhou a classe 'concluida' e o ícone ficou vermelho)
       cy.wrap($tarefa).should('have.class', 'concluida')
       cy.wrap($tarefa).find('i').should('have.class', 'text-red-500')
 
-      // 3. Clica de novo na mesma tarefa para desmarcar
+
+      
       cy.wrap($tarefa).click()
 
-      // 4. Verifica se desmarcou (se perdeu a classe e o ícone voltou ao normal)
+
+      
       cy.wrap($tarefa).should('not.have.class', 'concluida')
       cy.wrap($tarefa).find('i').should('not.have.class', 'text-red-500')
     })
